68d1bf9629dcdda5e5eea96e774e5ff1f3d205d2
Wassim <wassim.bacha@adventori.com>
2025-01-14 15:02:11 +0100

    Basic map & navmesh

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public class testest : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = GameObject.Find("Player");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
     }
 }
